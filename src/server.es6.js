5dfd4be12a81f399533aaf0923e84d91f9815b23
Jack Lawson <ajacksified@gmail.com>
2016-02-25 16:14:48 -0800

    [WIP] needs layout readded somehow? also nukes data loading. hmm.

--- src/server.es6.js
+++ src/server.es6.js
@@ -26,37 +26,17 @@ class ServerReactApp extends App {
   }
 
   async render (ctx) {
-    if (typeof ctx.body === 'function') {
-      let Layout = ctx.layout;
-      let props = ctx.props;
-      ctx.type = 'text/html; charset=utf-8';
+    //todo figure out html template
+    ctx.type = 'text/html; charset=utf-8';
 
-      try {
-        if (ctx.staticMarkup) {
-          let layout = ReactDOM.renderToStaticMarkup(<Layout {...props } />);
-          let body = ReactDOM.renderToString(ctx.body(props));
-
-          ctx.body = layout.replace(/!!CONTENT!!/, body);
-        } else {
-          ctx.body = ReactDOM.renderToStaticMarkup(
-            <Layout {...props}>
-              { ctx.body(props) }
-            </Layout>
-          );
-        }
-      } catch (e) {
-        ctx.props.app.error(e, ctx, ctx.props.app);
-        await ctx.props.app.render(ctx);
+    try {
+      if (React.isValidElement(ctx.body)) {
+        let body = ReactDOM.renderToStaticMarkup(ctx.body);
+        ctx.body = body;//layout.replace(/!!CONTENT!!/, body);
       }
-    }
-  }
-
-  async loadData(ctx) {
-    // ctx.props.data is a map; pass in its keys as an array of promises
-    if (ctx.props.data) {
-      return Promise.all([...ctx.props.data.values()]);
-    } else {
-      return Promise.resolve();
+    } catch (e) {
+      ctx.props.app.error(e, ctx, ctx.props.app);
+      await ctx.props.app.render(ctx);
     }
   }
 
@@ -67,60 +47,21 @@ class ServerReactApp extends App {
       .replace(/>/g, '\\u003E');
   }
 
-  static serverRender (app, formatProps) {
-    return async function (ctx) {
+  static serverRender (app) {
+    return async function (ctx, next) {
       ctx.timings = {};
 
       if (ctx.accepts('html')) {
         let routeStart = Date.now();
-        await app.route(ctx);
+        await app.route(ctx, next);
         ctx.timings.route = Date.now() - routeStart;
       }
 
-      if (typeof ctx.body === 'function') {
-        // Load all the data required for the request before the server renders
-        let data;
-        ctx.props = ctx.props || {};
+      let renderStart = Date.now();
+      await app.render(ctx);
+      ctx.timings.render = Date.now() - renderStart;
 
-        try {
-          let dataStart = Date.now();
-          data = await app.loadData(ctx);
-          ctx.timings.data = Date.now() - dataStart;
-        } catch (e) {
-          app.error(e, ctx, app);
-        }
-
-        ctx.props.dataCache = {};
-
-        if (data) {
-          // The entries are in the same order as when we fired off the promises;
-          // load the data from the response array.
-          let i = 0;
-          for (let [key] of ctx.props.data.entries()) {
-            ctx.props.dataCache[key] = data[i];
-            i++;
-          }
-        }
-
-        if (ctx.preServerRender) {
-          const preServerRender = ctx.preServerRender(ctx);
-
-          // If you explicitly return `false`, don't continue the render.
-          if (preServerRender === false) {
-            return;
-          }
-        }
-
-        let renderStart = Date.now();
-        await app.render(ctx);
-        ctx.timings.render = Date.now() - renderStart;
-
-        if (formatProps) {
-          ctx.props = formatProps(ctx.props);
-        }
-
-        await app.injectBootstrap(ctx, app.config.formatBootstrap);
-      }
+      //await app.injectBootstrap(ctx, app.config.formatBootstrap);
     };
   }
 }
